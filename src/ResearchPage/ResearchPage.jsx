7afbb0dd97b98dcab8cb37074b1a64c0f9345151
ronci <jyj9647@naver.com>
2021-12-03 18:30:33 +0900

    feat: make reasearch_page

---
+++ src/ResearchPage/ResearchPage.jsx
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+import { Button } from '@mui/material';
+import Header from '../Header/Header.jsx';
+import Footer from '../Footer/Footer.jsx';
+
+import './ResearchPage.css';
+
+export default class ResearchPage extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            question: [
+                "1. 문제", 
+                "2. 문제", 
+                "3. 문제", 
+                "4. 문제", 
+                "5. 문제", 
+                "6. 문제", 
+            ],
+            questionIndex : 0,
+            ans: [],
+        }
+    }
+
+    handleAddAns = (e, user_ans) => {
+        let nextquestionIndex = this.state.questionIndex + 1;
+        this.setState({
+            questionIndex: nextquestionIndex,
+            ans: [...this.state.ans, user_ans]
+        })
+    }
+
+    render() {
+        let {question, questionIndex} = this.state;
+
+        return (<div>
+        <Header />
+        <div className="research_page_wrap">
+            <div className="research_page">
+                <div className="question">{question[questionIndex]}</div>
+                <div class="ans">
+                    <Button className="ans_button" onClick={(e) => this.handleAddAns(e, 1)}>YES</Button>
+                    <div className="ans_blank"></div>
+                    <Button className="ans_button" onClick={(e) => this.handleAddAns(e, 2)}>NO</Button>
+                </div>
+            </div>
+        </div>
+        <Footer />
+        </div>)
+    }
+}
