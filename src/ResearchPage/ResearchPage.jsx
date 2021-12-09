f6f194c56ba20ef5a615b67fd69ebc17808f6055
ronci <jyj9647@naver.com>
2021-12-09 17:29:43 +0900

    feat: add loader

--- src/ResearchPage/ResearchPage.jsx
+++ src/ResearchPage/ResearchPage.jsx
@@ -25,6 +25,12 @@ export default class ResearchPage extends Component {
 
     handleAddAns = (e, user_ans) => {
         let nextquestionIndex = this.state.questionIndex + 1;
+        if (this.state.question.length == nextquestionIndex) {
+            this.setState({
+                ans: [...this.state.ans, user_ans]
+            })
+            // document.location.href = "/";
+        }
         this.setState({
             questionIndex: nextquestionIndex,
             ans: [...this.state.ans, user_ans]
@@ -38,12 +44,18 @@ export default class ResearchPage extends Component {
         <Header />
         <div className="research_page_wrap">
             <div className="research_page">
-                <div className="question">{question[questionIndex]}</div>
-                <div class="ans">
-                    <Button className="ans_button" onClick={(e) => this.handleAddAns(e, 1)}>YES</Button>
-                    <div className="ans_blank"></div>
-                    <Button className="ans_button" onClick={(e) => this.handleAddAns(e, 2)}>NO</Button>
+            { questionIndex != question.length
+              ? <div>
+                    <div className="question">{question[questionIndex]}</div>
+                    <div class="ans">
+                        <Button className="ans_button" onClick={(e) => this.handleAddAns(e, 1)}>YES</Button>
+                        <div className="ans_blank"></div>
+                        <Button className="ans_button" onClick={(e) => this.handleAddAns(e, 2)}>NO</Button>
+                    </div>
                 </div>
+              : <div className="loader">
+                </div>
+            }
             </div>
         </div>
         <Footer />
