aad17c54c88b975191fa682839b5e4ad795af932
ronci <jyj9647@naver.com>
2021-12-17 18:22:16 +0900

    design: add wrap

--- src/RecommendPage/RecommendPage.jsx
+++ src/RecommendPage/RecommendPage.jsx
@@ -5,12 +5,16 @@ import RecommmendMedia from "./RecommendMedia";
 import Top10Video from "../MainPage/Top10Video";
 import Footer from '../Footer/Footer.jsx';
 
+import './CSS/RecommendPage.css';
+
 export default class RecommendPage extends Component {
     render() {
         return (<div>
             <Header />
-            <RecommmendMedia />
-            <Top10Video />
+            <div className='recommend_page_wrap'>
+                <RecommmendMedia />
+                <Top10Video />
+            </div>
             <Footer />
         </div>)
     }
